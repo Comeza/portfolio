3e9628f75fc9efb677084f0faa04310a5042d056
Aaron Geiger <aaron@vobe.io>
2021-07-06 00:54:22 +0200

    v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Style from 'style/modules/app.module.sass';
+import { ReactComponent as WaveSVG } from 'assets/wave.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div id={Style.APP}>
+        <div className={Style.nameContainer}>
+          <span className={Style.name}>AARON&nbsp;</span>
+          <span className={Style.surname}>GEIGER</span>
+          <WaveSVG className={Style.wave} />
+        </div>
+
+        <div className={Style.text}>
+          <span>web developer.</span>
+          <span>software developer.</span>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;

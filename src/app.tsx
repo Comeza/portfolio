0886bc0b9dc96f1622b4ab130c2f0d7fac5e7ba9
Aaron Geiger <aaron@vobe.io>
2021-08-03 19:38:07 +0200

    "Updated colors for links and added favicon2

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import COLORS from "assets/colors.json"
 import Style from 'style/modules/app.module.sass';
 import { ReactComponent as GithubSVG } from 'assets/github.svg';
 import { ReactComponent as MailSVG } from 'assets/mail.svg';
 import { ReactComponent as WaveSVG } from 'assets/wave.svg';
 
 export default class App extends Component {
+
   render() {
     return (
       <div id={Style.APP}>
@@ -16,9 +18,10 @@ export default class App extends Component {
 
         <div className={Style.contentContainer}>
           <div className={Style.linkContainer}>
-            <a href="https://github.com/comeza" target="_blank" rel="noreferrer"><GithubSVG/></a>
-            <a href="mailto:aaron@geigr.io"><MailSVG/></a>
+            <a href="https://github.com/comeza" target="_blank" rel="noreferrer"><GithubSVG fill={COLORS.foreground}/></a>
+            <a href="mailto:aaron@geigr.io"><MailSVG fill={COLORS.foreground}/></a>
           </div>
+
           <div className={Style.text}>
             <span>web developer.</span>
             <span>software developer.</span>

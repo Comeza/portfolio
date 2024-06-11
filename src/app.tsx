9def4054a4256e9d661fcad745cd75901d6980df
Aaron Geiger <aaron@geigr.dev>
2024-06-12 00:59:46 +0200

    Update quotes

--- src/app.tsx
+++ src/app.tsx
@@ -6,50 +6,53 @@ import quotes from "assets/quotes.json";
 import { Shader, ShaderProps } from "Shader";
 
 interface Link {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 }
 
 export const App = () => {
-  const quote = quotes[new Date().getDate() % quotes.length];
+    const quote = quotes[new Date().getDate() % quotes.length];
 
-  const shader: ShaderProps = {
-    vertexShader,
-    fragmentShader,
-    canvasProps: {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    },
-  };
+    const shader: ShaderProps = {
+        vertexShader,
+        fragmentShader,
+        canvasProps: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+        },
+    };
 
-  return (
-    <div>
-      <Shader {...shader} />
-      <div className="d-flex">
-        <div className="v-line" />
-        <div className="d-flex flex-v">
-          <span className="f-switzer name-title">
-            AARON
-            <br />
-            GEIGER
-          </span>
+    return (
+        <div>
+            <Shader {...shader} />
+            <div className="d-flex">
+                <div className="v-line" />
+                <div className="d-flex flex-v">
+                    <span className="f-switzer name-title">
+                        AARON
+                        <br />
+                        GEIGER
+                    </span>
 
-          <div className="d-flex flex-v box">
-            <span>student.</span>
-            <span>developer.</span>
-          </div>
+                    <div className="d-flex flex-v box">
+                        <span>student.</span>
+                        <span>developer.</span>
+                    </div>
 
-          <div className="d-flex flex-v box">
-            {(links as Link[]).map((contact: Link) => (
-              <a href={contact.url} key={contact.url}>
-                {contact.name}
-              </a>
-            ))}
-          </div>
+                    <div className="d-flex flex-v box">
+                        {(links as Link[]).map((contact: Link) => (
+                            <a href={contact.url} key={contact.url}>
+                                {contact.name}
+                            </a>
+                        ))}
+                    </div>
 
-          <span className="box quote">{quote}</span>
+                    <div className="box quote">
+                        <span>{quote.quote}</span>
+                        <span style={{float: "right"}}>— {quote.source}</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
